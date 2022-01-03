2ff05fddb899d55312c47a412e3de7b6de1288a5
Lakpa Sange Sherpa <sangesherpa1215@gmail.com>
2022-01-03 16:51:43 +0545

    Module Basics

---
+++ js/index.js
@@ -0,0 +1,9 @@
+import add from './modules/add.js';
+import sub from './modules/sub.js';
+import multiply from './modules/multiply.js';
+import divide from './modules/divide.js';
+
+console.log("Sum is: " + add(10,10));
+console.log("Dif is: " + sub(10,10));
+console.log("Pro is: " + multiply(10,10));
+console.log("Div is: " + divide(10,10));
