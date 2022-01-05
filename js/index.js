18421e1aaab9df379fec3e51da9c52e36b106f38
Lakpa Sange Sherpa <sangesherpa1215@gmail.com>
2022-01-05 16:09:24 +0545

    Starting OOP In JavaScript

---
+++ js/index.js
@@ -0,0 +1,11 @@
+let names = {
+    f_name: "Sange",
+    l_name: "Sherpa"
+}
+
+console.log(names.f_name);
+console.log(names.hasOwnProperty('m_name'));
+console.log(names);
+
+// let array= [1,2,3]
+// console.log(array);
