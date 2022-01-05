e2e661f277668159bf9ef538fec2b5f6dfd2593d
Lakpa Sange Sherpa <sangesherpa1215@gmail.com>
2022-01-05 17:15:23 +0545

    Concept of Objects

--- js/index.js
+++ js/index.js
@@ -1,11 +1,32 @@
-let names = {
-    f_name: "Sange",
-    l_name: "Sherpa"
-}
+// let names = {
+//     f_name: "Sange",
+//     l_name: "Sherpa"
+// }
 
-console.log(names.f_name);
-console.log(names.hasOwnProperty('m_name'));
-console.log(names);
+// console.log(names.f_name);
+// console.log(names.hasOwnProperty('m_name'));
+// console.log(names);
 
 // let array= [1,2,3]
 // console.log(array);
+
+// let constructorObject = {
+//     speak: () => {
+//         return "I am a dog"
+//     }
+// }
+
+function DogObject(name, age) {
+    // let dog = Object.create(constructorObject);
+    this.name = name;
+    this.age  = age;
+
+    // return dog
+}
+
+DogObject.prototype.speak = () => {
+    return "I am not a dog."
+}
+
+let bingo = new DogObject("Bingo", 24)
+console.log(bingo.speak());
